e632c9380864894b162d3f6890f2affaadb65c99
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-01-25 00:14:45 -0200

    9.3 o conteudo principal

--- garconapp/www/js/app.js
+++ garconapp/www/js/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    $('.modal').modal();
+
     $('.collection').on('click', '.collection-item', function(){
         var $badge = $('.badge', this);
         if ($badge.length === 0) {
@@ -7,9 +9,16 @@ $(document).ready(function(){
         $badge.text(parseInt($badge.text()) + 1);
 
         var nomeProduto = this.firstChild.textContent;
-        Materialize.toast(nomeProduto + ' adicionado', 1000);
+        Materialize.toast(nomeProduto + ' adicionado', 500);
     });
 
-    $('.modal').modal();
+    $('#confirmar').on('click', function() {
+        var texto = "";
+        $('.badge').parent().each(function(){
+            console.log(this);
+            texto += this.firstChild.textContent + ': ';
+            texto += this.lastChild.textContent + ', ';
+        });
+        $('#resumo').empty().text(texto);
+    });
 });
-
