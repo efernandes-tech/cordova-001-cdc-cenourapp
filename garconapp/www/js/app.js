a42c5471eeb6a5dac5c1263214acec616d31b8c5
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-01-24 23:39:16 -0200

    9.1 anotando o pedido

---
+++ garconapp/www/js/app.js
@@ -0,0 +1,7 @@
+$('.collection').on('click', '.collection-item', function(){
+    var $badge = $('.badge', this);
+    if ($badge.length === 0) {
+        $badge = $('<span class="badge brown-text">0</span>').appendTo(this);
+    }
+    $badge.text(parseInt($badge.text()) + 1);
+});
