57c113b6103da69dab3a3d5109761831f2aea366
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-01-30 13:45:34 -0200

    13.6 configurando a status bar

--- garconapp/platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
+++ garconapp/platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+    "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+    "pluginId": "phonegap-plugin-barcodescanner",
+    "clobbers": [
+      "cordova.plugins.barcodeScanner"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "phonegap-plugin-barcodescanner": "7.0.2"
 };
 // BOTTOM OF METADATA
 });
