a23b2033df544cee66037ac3c1640aa3bc8c1a0d
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-01-30 12:29:52 -0200

    13.4 integrando na app

--- garconapp/www/js/app.js
+++ garconapp/www/js/app.js
@@ -31,4 +31,15 @@ $(document).ready(function(){
         $('#numero-mesa').val('');
         $('.badge').remove();
     });
+
+    $('.scan-qrcode').on('click', function(){
+        cordova.plugins.barcodeScanner.scan(function (resultado) {
+            if (resultado.text) {
+                Materialize.toast('Mesa ' + resultado.text, 2000);
+                $('#numero-mesa').val(resultado.text);
+            }
+        }, function (error) {
+            Materialize.toast('Erro: ' + error, 3000, 'red-text');
+        });
+    });
 });
