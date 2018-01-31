9da5df93ab008a9d7e60655800427d783e0730e5
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-01-30 22:32:16 -0200

    18.3 solucao offline-first

--- pedidapp/www/js/services.js
+++ pedidapp/www/js/services.js
@@ -1,11 +1,25 @@
 angular.module('starter')
 .service('ProdutosService', function($http, $q) {
     var url = 'http://cozinhapp.sergiolopes.org/produtos';
+
+    // Sempre dispara o serviço pra checar dados mais recentes.
+    var promise = $http.get(url).then(function(response){
+        var json = JSON.stringify(response.data);
+        localStorage.setItem('cache', json);
+        return response.data;
+    });
+
+    // Procura no localStorage.
+    var cache = localStorage.getItem('cache');
+    if (cache != null) {
+        promise = $q(function(resolve, reject) {
+            resolve(JSON.parse(cache));
+        });
+    }
+
     return {
-        lista: function(){
-            return $http.get(url).then(function(response){
-                return response.data;
-            });
+        lista: function() {
+            return promise;
         }
     };
 });
